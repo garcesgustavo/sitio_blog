80d10a1cd71472e64d3986b8ba861a351b803ac1
garcesgustavo <garcesgustavo@gmail.com>
2022-05-28 08:59:08 -0300

    agregados de posteo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { method } = require("lodash");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,8 +21,11 @@ app.use(express.static("public"));
 
 let posts = [];
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
-  console.log(posts);
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts
+  });
+  console.log(req.params. testing);
 });
 
 app.get("/about", function (req, res) {
@@ -40,13 +43,21 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
   var post = {
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
   };
   posts.push(post);
 
   res.redirect("/");
 });
-
+app.get("/post/:postName", function(req,res) {
+ var namePost = _.lowerCase(req.params.postName);
+ 
+posts.forEach(function(post) {
+   if (namePost === _.lowerCase(post.title)) {
+     console.log("match found");
+   } else {console.log("No match found")};
+ } );
+});
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
